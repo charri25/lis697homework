9f09d973ea8e1c0ebf3e33e25cfc6aa61859fdcc
charri25 <Caitlin@Caitlin-Harringtons-MacBook.local>
2015-09-01 21:13:34 -0400

    class practice

---
+++ test1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php //test1.php
+$first_name="Caitlin";
+$middle_name="Ann";
+$surname="Harrington";
+$age=27; 
+echo "$surname, $first_name $middle_name ($age)";
+print "<ul>
+<li>Harrington, Caitlin Ann (27)</li>
+</ul>";
+//$name= array ('Harrington','Caitlin','Ann');
+$name= $surname .' '. $first_name .' ' .$middle_name . ' ('.$age.')';
+echo "<br>".$name."</br>";
+$old= $age + 5;
+echo "<br>".$old."</br>";
+
+$months= $age * 12;
+echo "<br>".$months."</br>";
+
+$very_old= 2034-2015;
+echo $age+$very_old;
+
+echo "<br>".++$age."</br>";
+?>
+</body>
+</html>
